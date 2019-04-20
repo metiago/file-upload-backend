1d62a4d5a1471f5234b57b53834e53263ad54c97
tiago <ctw00244@criticaltechworks.com>
2019-04-20 13:09:38 +0100

    changed func name

--- common/helper/security.go
+++ common/helper/security.go
@@ -7,7 +7,7 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
-func HashPassword(password string) (string, error) {
+func EncryptPassword(password string) (string, error) {
 	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
 	return string(bytes), err
 }
